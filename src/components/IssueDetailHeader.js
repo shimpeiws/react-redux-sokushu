c632278c0de5da8ecd4fab4a40f36d64d26ad6ce
Mamoru Amano <booktower103@gmail.com>
2016-06-09 16:31:02 +0900

    style

--- src/components/IssueDetailHeader.js
+++ src/components/IssueDetailHeader.js
@@ -26,7 +26,7 @@ class IssueDetailHeader extends Component {
     const { issue } = this.props
     return (
       <div styleName="base">
-        <div>
+        <div styleName={issue.status === STATE.OPEN ? 'state-open' : 'state-close'}>
           {
             issue.status === STATE.OPEN ?
             <i className="fa fa-check-circle-o" /> :
